6d459db3c44f5e76caf29b292b81f95f43381b51
pulkitk1234 <pulkitk1403@gmail.com>
2021-08-03 17:56:16 +0530

    Before Validation

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { User } from './user';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'tdf';
+  topics=['Angular','React','Vue']
+
+  userModel= new User('Rob','abc@com',334455,'','morning',true);
+}
