776be8936d0d621be139d0c814ce8e8c2c83b867
pulkitk1234 <pulkitk1403@gmail.com>
2021-08-05 13:33:48 +0530

    Till-Simple validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,53 @@
 import { Component } from '@angular/core';
-import { User } from './user';
-
+import { FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'tdf';
-  topics=['Angular','React','Vue'];
-  // first= "heyy";
-  
 
-  userModel= new User('','abc@com',334455,'','morning',true);
+  // registrationForm= new FormGroup({
+  //   userName: new FormControl('Pulkit'),
+  //   password: new FormControl(''),
+  //   confirmPassword: new FormControl(''),
+  //   address: new FormControl({
+  //     city: new FormControl(''),
+  //     state: new FormControl(''),
+  //     postalCode: new FormControl('')
+
+
+  //   })
+  // });
+
+  get userName(){
+    return this.registrationForm.get('userName');
+  }
+
+  constructor(private fb: FormBuilder) { }
+
+  registrationForm = this.fb.group({
+    userName: ['', [Validators.required, Validators.minLength(4)]],
+    password: [''],
+    confirmPassword: [''],
+    address: this.fb.group({
+      city: [''],
+      state: [''],
+      postalCode: ['']
+    })
+  });
+
+  loadApiData() {
+    this.registrationForm.patchValue({
+      userName: 'Bruce',
+      password: 'test',
+      confirmPassword: 'test',
+      address: {
+        city: 'City',
+        state: 'State',
+        postalCode: '123456',
+      }
+    });
+  }
 }
